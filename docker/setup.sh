731e5b6115c454489eec5884d24118880e40363a
Developer JON <starkeyjon@protonmail.com>
2022-11-04 14:45:45 +0000

    updating vscode and docker setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-DOCKER_IMAGE=$(docker ps --filter name=SCAFFOLD_ETH -q)
+DOCKER_IMAGE=$(docker ps --filter name=ETH_CHESS -q)
 
 if [ "$1" = "start" ]; then
   # Run Docker container
   # to run the frontend on a different port add the "-e PORT=8080" parameter and change "-p 8080:8080" one.
   [ -z "$DOCKER_IMAGE" ] && docker run \
-    --name SCAFFOLD_ETH \
-    -v `pwd`:/opt/scaffold-eth \
-    -w /opt/scaffold-eth \
+    --name ETH_CHESS \
+    -v `pwd`:/opt/eth_chess \
+    -w /opt/eth_chess \
     -p 3000:3000 \
+    -p 8080:8080 \
     -p 8545:8545 \
-    -dt node:16 || docker restart SCAFFOLD_ETH
+    -dt node:18 || docker restart ETH_CHESS
 
-  docker exec -ti SCAFFOLD_ETH bash -c "yarn install"
-  docker exec -dt SCAFFOLD_ETH bash -c "yarn chain"
+  docker exec -ti ETH_CHESS bash -c "yarn install"
+  docker exec -dt ETH_CHESS bash -c "yarn chain"
+  docker exec -dt ETH_CHESS bash -c "yarn server"
   sleep 5
-  docker exec -ti SCAFFOLD_ETH bash -c "yarn deploy"
-  docker exec -dt SCAFFOLD_ETH bash -c "yarn start"
+  docker exec -ti ETH_CHESS bash -c "yarn deploy"
+  docker exec -dt ETH_CHESS bash -c "yarn start"
 else
   if [ "$1" = "deploy" ]; then
     [ -z "$DOCKER_IMAGE" ] && echo "Container does not exist. Run the script with 'start' before running it with the 'deploy' option." \
-      || docker exec -ti SCAFFOLD_ETH bash -c "yarn deploy"
+      || docker exec -ti ETH_CHESS bash -c "yarn deploy"
   else
     echo "Invalid command. Choose 'start' or 'deploy'."
   fi
