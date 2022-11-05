a15095610b32a2b381e460b98eb5a81fb58a67e1
Developer JON <starkeyjon@protonmail.com>
2022-11-05 17:37:15 -0400

    connecting deployed test contracts

--- packages/react-app/src/components/NotificationComponents/NotifyMatch.jsx
+++ packages/react-app/src/components/NotificationComponents/NotifyMatch.jsx
@@ -4,12 +4,12 @@ import React from "react";
 export const NotifyMatch = ({ showNotifyMatch, setShowNotifyMatch, matchId }) => {
     return (
         <>
-        <Modal 
-            title="Match started!"
-            visible={showNotifyMatch}
-            onCancel={() => setShowNotifyMatch(false)}
-            onOk={() => window.location.replace(`/match/${matchId}`)}
-        >
+            <Modal
+                title="Match started!"
+                visible={showNotifyMatch}
+                onCancel={() => setShowNotifyMatch(false)}
+                onOk={() => window.location.replace(`/match/${matchId}`)}
+            >
             </Modal></>
     )
 }
@@ -17,12 +17,12 @@ export const NotifyMatch = ({ showNotifyMatch, setShowNotifyMatch, matchId }) =>
 export const NotifyDeathMatch = ({ showNotifyDeathMatch, setShowNotifyDeathMatch, matchId }) => {
     return (
         <>
-        <Modal 
-            title="DeathMatch started!"
-            visible={showNotifyDeathMatch}
-            onCancel={() => setShowNotifyDeathMatch(false)}
-            onOk={() => window.location.replace(`/deathmatch/${matchId}`)}
-        >
+            <Modal
+                title="DeathMatch started!"
+                visible={showNotifyDeathMatch}
+                onCancel={() => setShowNotifyDeathMatch(false)}
+                onOk={() => window.location.replace(`/deathmatch/${matchId}`)}
+            >
             </Modal></>
     )
 }
