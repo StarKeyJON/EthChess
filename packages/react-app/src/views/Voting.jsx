ea0e3a5e58408b0f4bca8d76807330c7ff40db96
Developer JON <starkeyjon@protonmail.com>
2022-11-15 10:17:55 -0500

    updating graphQL

--- packages/react-app/src/views/Voting.jsx
+++ packages/react-app/src/views/Voting.jsx
@@ -1,155 +1,54 @@
-import { SearchOutlined } from "@ant-design/icons";
-import { Button, Card, Input, Popover, Space, Table } from "antd";
-import React, { useRef, useState } from "react";
+import { Card, Popover, Table } from "antd";
+import React from "react";
 import { FaInfoCircle, FaVoteYea } from "react-icons/fa";
-import Highlighter from "react-highlight-words";
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
-const Voting = ({ players }) => {
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
-  const searchInput = useRef(null);
-
+const Voting = ({ address, tx, writeContracts, readContracts, gun, gunUser }) => {
   const matchQ = `
   {
-      disputes(where: onGoing){
-        id
-        matchId
-        player1 {
-          id
-        }
-        player2 {
-          id
-        }
-        startTime
-        startHash
-        endHash
-        p1Amount
-        p2Amount
-        inProgress
-      }
-    }`;
+    disputes(where: {onGoing: true}){
+      id
+      matchId
+      claimStart
+      security
+      disputer
+      startHash
+      endHash
+      contestedHash
+      tally
+      votedFor
+      votedAgainst
+      onGoing
+    }
+  }`;
+
   const MATCH_GQL = gql(matchQ);
   const { loading, matchData } = useQuery(MATCH_GQL, { pollInterval: 2500 });
 
-  const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = clearFilters => {
-    clearFilters();
-    setSearchText("");
-  };
-
-  const getColumnSearchProps = dataIndex => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-      <div
-        style={{
-          padding: 8,
-        }}
-      >
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{
-            marginBottom: 8,
-            display: "block",
-          }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{
-              width: 90,
-            }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{
-              width: 90,
-            }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({
-                closeDropdown: false,
-              });
-              setSearchText(selectedKeys[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            Filter
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: filtered => (
-      <SearchOutlined
-        style={{
-          color: filtered ? "#1890ff" : undefined,
-        }}
-      />
-    ),
-    onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    onFilterDropdownOpenChange: visible => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: text =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{
-            backgroundColor: "#ffc069",
-            padding: 0,
-          }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ""}
-        />
-      ) : (
-        text
-      ),
-  });
-
   const columns = [
     {
-      title: "GameId",
-      dataIndex: "gameId",
-      key: "gameId",
+      title: "Match ID",
+      dataIndex: "matchId",
+      key: "matchId",
       width: "20%",
     },
     {
       title: "Claim",
-      dataIndex: "claimHash",
-      key: "claimHash",
+      dataIndex: "endHash",
+      key: "endHash",
       width: "20%",
     },
     {
       title: "Dispute",
-      dataIndex: "disputeHash",
-      key: "disputeHash",
+      dataIndex: "contestedHash",
+      key: "contestedHash",
       width: "20%",
     },
     {
-      title: "Votes",
-      dataIndex: "votes",
-      key: "votes",
+      title: "Outcome",
+      dataIndex: "tally",
+      key: "tally",
       width: "20%",
     },
     {
@@ -157,15 +56,16 @@ const Voting = ({ players }) => {
       dataIndex: "security",
       key: "security",
       width: "20%",
-      ...getColumnSearchProps("security"),
     },
   ];
-
 
   return (
     <>
       <div>
         <Card>
+          <div>
+            Mint an ETH-Chess NFT for 0.01 ETH <Link to="/mint">Mint</Link>
+          </div>
           <div style={{ marginTop: 50 }}>
             <h1>
               <FaVoteYea /> Match Disputes{" "}
